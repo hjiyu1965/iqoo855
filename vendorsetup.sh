b6e974e1e963618463dc4b140f0d1894c3552ae2
hjiyu1965 <168966132+hjiyu1965@users.noreply.github.com>
2024-06-10 03:40:27 +0800

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for vivo V1936A (PD1936)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_PD1936-user
+add_lunch_combo omni_PD1936-userdebug
+add_lunch_combo omni_PD1936-eng
